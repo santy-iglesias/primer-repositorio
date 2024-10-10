bbb55dc970a8c8854ee948feec6a7a58b9508a79
Santiago Iglesias <santi.iglesiasfuentes@gmail.com>
2024-10-10 14:29:03 -0300

    Subiendo el archivo de C++

---
+++ codigo.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char nombre[] = "Santiago";
+
+    cout << "Hola " << nombre << "!" << endl;
+    cout << "¿Como estas?" << endl;
+
+    return 0;
+}
